3ef1cc3d69425dd170bb6d887fcf86f04eae5734
gabrielripardo <gts.senna@gmail.com>
2022-10-14 00:19:24 -0300

    first--storybook+tailwind_tokens

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+export function App() {  
+  return (
+    <>
+    <h1 className='text-gray-200 text-2xl'>Hello World</h1>
+    <button className='bg-cyan-500'>Enviar</button>
+    </>
+  )
+}
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#E1E1E6',
+      'gray-0': '#fff',
+
+      'black': '#000',
+
+      'cyan-300': '#81D8F7',
+      'cyan-500': '#61DAFB'
+    },
+
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
